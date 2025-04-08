fa631173a1107ac2f307241bd8a1146a9ad11d1e
rgurylev <rgurylev@gmail.com>
2025-04-08 17:17:59 +0300

    #1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="finpymist",  # package name
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),  # automatically find sub-packages
     install_requires=[],  # put dependencies here if needed
     author="roman gurylev",
