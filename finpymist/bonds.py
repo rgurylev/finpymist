4d79848e71c6f33efeb25eb5e70a540516f5fd3a
rgurylev <rgurylev@gmail.com>
2025-11-07 21:59:49 +0300

    # удален мусорный код из bonds.ru

--- finpymist/bonds.py
+++ finpymist/bonds.py
@@ -1,7 +1,6 @@
 import os
 import logging
 import asyncio
-from bs4 import BeautifulSoup
 from tinkoff.invest import AsyncClient, Client
 from tinkoff.invest.schemas import MoneyValue
 from tinkoff.invest.schemas import RiskLevel
@@ -83,51 +82,9 @@ class Bond:
         result = {item: getattr(self, item, None) for item in items}
         return result
 
-    """
-    async def get_price(self):
-        async with AsyncClient(TOKEN) as client:
-            try:
-                price, self.currate = await asyncio.gather(
-                    client.market_data.get_last_prices(figi=[self.figi]),
-                    get_rate_async(self.nominal.currency)
-                )
-                price = price.last_prices[0].price
-                self.price = (price.units + price.nano / 10 ** 9) * self.nominal.units / 100
-            except Exception as e:
-                logger.error(f'Ошибка определения цены облигации {self.ticker}: {e} ')
-
-    async def get_coupons(self):
-        async with AsyncClient(TOKEN) as client:
-            try:
-
-                  self.coupons = await client.instruments.get_bond_coupons(figi=self.figi, from_=datetime.today(),
-                                                        to=datetime(2050, 12, 31))
-                  #self.coupons = coupons
-            except Exception as e:
-                logger.error(f'Ошибка получения графика выплаты купонов по облигации {self.ticker}: {e} ')
-    async def get_rank(self):
-        url = 'https://smart-lab.ru/q/bonds/' + self.ticker
-        print(f'url: {url}')
-        try:
-            html = await get_html(url)
-            soup = BeautifulSoup(html, 'html.parser')
-
-        # Find the element containing the label
-            label = soup.find('div', string='Кредитный рейтинг')
-
-        # Find the next sibling that contains the value
-            if label:
-                value_container = label.find_next_sibling('div')
-                rating_text = value_container.select_one('.linear-progress-bar__text')
-                if rating_text:
-                    self.rank = rating_text.get_text(strip=True)
-        except Exception as e:
-           logger.error (f'Ошибка определения кредитного рейтинга облигации {self.ticker}: {e}')
-    """
-
     async def calc_rate(self):
         end_date = min (self.maturity_date, self.oferta_date if self.oferta_date else self.maturity_date)
-        coupons_last_date = None
+        #coupons_last_date = None
         # if self.price == None:
         #     await self.get_price()
         # if self.coupons == None:
@@ -163,21 +120,12 @@ class Bond:
             self.rate = xirr(values, operdates, 365)
             #await asyncio.sleep(1 )
             self.cash_flow = (operdates, values)
-            #for d, v in zip(operdates, values):
-            #    logger.debug (f'{d}      {v}')
-
 
 # функции  получения атрибутов облигации
 
 async def get_price(bond):
         async with AsyncClient(TOKEN) as client:
             try:
-                #price, self.currate = await asyncio.gather(
-                #    client.market_data.get_last_prices(figi=[self.figi]),
-                #    get_rate_async(self.nominal.currency)
-                #)
-                #price = price.last_prices[0].price
-                #self.price = (price.units + price.nano / 10 ** 9) * self.nominal.units / 100
                 price = await client.market_data.get_last_prices(figi=[bond.figi])
                 price = price.last_prices[0].price
                 price = (price.units + price.nano / 10 ** 9) * bond.nominal.units / 100
@@ -198,130 +146,10 @@ async def get_coupons(bond):
                         logger.error(f'Ошибка получения графика выплаты купонов по облигации {bond.ticker}: {e} ')
                         return None
 
-'''
-async def get_rank(isin):
-        url = 'https://smart-lab.ru/q/bonds/' + isin
-        logger.debug(f'get_rank: {url}')
-        try:
-            html = await get_html(url)
-            soup = BeautifulSoup(html, 'html.parser')
-
-        # Find the element containing the label
-            label = soup.find('div', string='Кредитный рейтинг')
-
-        # Find the next sibling that contains the value
-            if label:
-                value_container = label.find_next_sibling('div')
-                rating_text = value_container.select_one('.linear-progress-bar__text')
-                if rating_text:
-                    rank = rating_text.get_text(strip=True)
-                    return (isin, rank)
-            return (isin, None)
-        except Exception as e:
-           logger.error (f'Ошибка определения кредитного рейтинга облигации {isin}: {e}')
-           return (isin, None)
-
-'''
-
 def m2f(money):
     return round (money.units + money.nano / 10 ** 9, 2)
 
-def paginate_list(items, page_size, page_number):
-    """Returns a slice of the list for the given page number and page size."""
-    start = (page_number - 1) * page_size
-    end = start + page_size
-    return items[start:end]
-
 async def bonds(max_days = 99999, min_rate = -1.0, limit = None):
-    bonds = []
-#    columns = ['name', 'ticker', 'figi', 'currency', 'days', 'price', 'rate', 'sector', 'risk', 'rank']
-    i = 0
-    #try:
-    async with AsyncClient(TOKEN) as client:
-        bonds_response = await client.instruments.bonds()
-    for b in bonds_response.instruments:
-            i+=1
-            #if not (b.isin=='RU000A10B1N3'): continue
-            if limit and i > limit: break
-            if 'ОФЗ' in b.name.upper(): continue
-            bond = Bond.create(b)
-            bond.price, coupons, bond.currate = await asyncio.gather(
-                get_price(bond),
-                get_coupons(bond),
-               # get_oferta(bond.ticker),
-                get_rate_async(bond.nominal.currency)
-            )
-            bond.coupons = sorted(coupons, key=lambda x: x.coupon_number)
-            #if bond.coupons[len(bond.coupons) - 1].pay_one_bond.units == 0:
-            #    logger.debug(f'Последний купон {bond.ticker} = 0. Ищем дату оферты')
-            #    bond.oferta_date, bond.call_date, bond.put_date = await get_oferta(bond.ticker)
-
-            if bond.risk in RISK and bond.days<=max_days and bond.currency=='rub' and bond.nominal.currency=='rub':
-                await bond.calc_rate()
-                if bond.rate == None or bond.rate >= min_rate:
-                    bonds.append(bond)
-                    logger.info (bond)
-                #if bond.rate == None or bond.rate>= min_rate:
-                    #await bond.get_rank()
-                    #list.append(bond.dict(columns))
-                #    i += 1
-                    #if i > 10: break
-                # if i%30 == 0: await asyncio.sleep(1 / 1000)
-            #print(i)
-
-    # добавляем кредитный рейтинг
-    ranks = await execute_func([x.ticker for x in bonds], page_size=10, delay=1, func_name=get_rank)
-    #print (ranks)
-    for  b, r in zip (sorted (bonds, key = lambda x: x.ticker) , sorted (ranks, key = lambda x: x[0])):
-       b.rank = r[1]
-       #print (b)
-    #await execute_method(bonds, page_size=5, method_name='calc_rate')
-    #bonds = [b for b in bonds if b.rate == None or b.rate>= min_rate]
-    #await execute_method(bonds, page_size=10, method_name='get_rank')
-
-    #page_size = 10
-    #total_pages = (len(bonds) + page_size - 1) // page_size
-    #№for page_number in range(1, total_pages + 1):
-    #    z = paginate_list(bonds, page_size, page_number)
-    #    await asyncio.gather(*[b.get_rank() for b in z])
-
-    #await asyncio.gather(*[b.get_rank() for b in bonds if b.rate == None or b.rate>= min_rate])
-    #list = [b.dict(columns) for b in bonds if bond.rate == None or bond.rate>= min_rate]
-    #list = [b.dict(columns) for b in bonds]
-    return bonds
-    #except Exception as e:
-    #   logger.error(f"Failed {repr(e)}")
-    #   return None
-
-async def bonds2(max_days = 99999, min_rate = -1.0):
-    list = []
-    columns = ['name', 'ticker', 'figi', 'currency', 'days', 'price', 'rate', 'sector', 'risk', 'rank']
-    try:
-       async with AsyncClient(TOKEN) as client:
-            bonds = await client.instruments.bonds()
-       i = 0
-       k = 0
-       n = len(bonds.instruments)
-       for b in bonds.instruments:
-                bond = Bond.create(b)
-                #days = (bond.maturity_date.replace(tzinfo=None) - TODAY).days
-                k += 1
-                if bond.risk in RISK and bond.days<=max_days and bond.currency=='rub' and bond.nominal.currency=='usd':
-                    await bond.calc_rate ()
-                    logger.info (b)
-                    if bond.rate == None or bond.rate>= min_rate:
-                        await bond.get_rank()
-                        list.append(bond.dict(columns))
-                        i += 1
-                        #if i > 10: break
-                    # if i%30 == 0: await asyncio.sleep(1 / 1000)
-                #print(i)
-       return list
-    except Exception as e:
-        logger.error(f"Failed {repr(e)}")
-        return None
-
-async def bonds3(max_days = 99999, min_rate = -1.0, limit = None):
     bonds = []
 #    columns = ['name', 'ticker', 'figi', 'currency', 'days', 'price', 'rate', 'sector', 'risk', 'rank']
     i = 0
@@ -355,13 +183,6 @@ async def bonds3(max_days = 99999, min_rate = -1.0, limit = None):
                     if bond.rate == None or bond.rate >= min_rate:
                         bonds.append(bond)
                         logger.info (bond)
-                    #if bond.rate == None or bond.rate>= min_rate:
-                        #await bond.get_rank()
-                        #list.append(bond.dict(columns))
-                    #    i += 1
-                        #if i > 10: break
-                    # if i%30 == 0: await asyncio.sleep(1 / 1000)
-                #print(i)
 
         # добавляем кредитный рейтинг
         ranks = await get_ranks([x.ticker for x in bonds])
@@ -369,25 +190,37 @@ async def bonds3(max_days = 99999, min_rate = -1.0, limit = None):
         for  b, r in zip (sorted (bonds, key = lambda x: x.ticker) , sorted (ranks, key = lambda x: x[0])):
            b.rank = r[1]
 
-        #await execute_method(bonds, page_size=5, method_name='calc_rate')
-        #bonds = [b for b in bonds if b.rate == None or b.rate>= min_rate]
-        #await execute_method(bonds, page_size=10, method_name='get_rank')
-
-        #page_size = 10
-        #total_pages = (len(bonds) + page_size - 1) // page_size
-        #№for page_number in range(1, total_pages + 1):
-        #    z = paginate_list(bonds, page_size, page_number)
-        #    await asyncio.gather(*[b.get_rank() for b in z])
-
-        #await asyncio.gather(*[b.get_rank() for b in bonds if b.rate == None or b.rate>= min_rate])
-        #list = [b.dict(columns) for b in bonds if bond.rate == None or bond.rate>= min_rate]
-        #list = [b.dict(columns) for b in bonds]
         return bonds
-    #except Exception as e:
-    #   logger.error(f"Failed {repr(e)}")
-    #   return None
     finally:
         await client.disconnect()
 
-if __name__ == "__main__":
-    asyncio.run(bonds())
+async def bonds2(max_days = 99999, min_rate = -1.0):
+    list = []
+    columns = ['name', 'ticker', 'figi', 'currency', 'days', 'price', 'rate', 'sector', 'risk', 'rank']
+    try:
+       async with AsyncClient(TOKEN) as client:
+            bonds = await client.instruments.bonds()
+       i = 0
+       k = 0
+       n = len(bonds.instruments)
+       for b in bonds.instruments:
+                bond = Bond.create(b)
+                #days = (bond.maturity_date.replace(tzinfo=None) - TODAY).days
+                k += 1
+                if bond.risk in RISK and bond.days<=max_days and bond.currency=='rub' and bond.nominal.currency=='usd':
+                    await bond.calc_rate ()
+                    logger.info (b)
+                    if bond.rate == None or bond.rate>= min_rate:
+                        await bond.get_rank()
+                        list.append(bond.dict(columns))
+                        i += 1
+                        #if i > 10: break
+                    # if i%30 == 0: await asyncio.sleep(1 / 1000)
+                #print(i)
+       return list
+    except Exception as e:
+        logger.error(f"Failed {repr(e)}")
+        return None
+
+#if __name__ == "__main__":
+#    asyncio.run(bonds())
